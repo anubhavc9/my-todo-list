94ad33a9e643714e65603909e2ffc1fe42ad44c1
anubhavc9 <anubhavc9@github.com>
2022-04-03 23:18:32 +0530

    added wilcard route & page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './MyComponents/about/about.component';
+import { PageNotFoundComponent } from './MyComponents/page-not-found/page-not-found.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 
 const routes: Routes = [
-  {path: '', component: TodosComponent},
-  {path: 'about', component: AboutComponent},
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './MyComponents/about/about.component';
 import { TodoService } from './services/todo.service';
 import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './MyComponents/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     TodosComponent,
     TodoItemComponent,
     AddTodoComponent,
-    AboutComponent
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
