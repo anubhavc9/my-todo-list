a9f2a13569a8105642d1d633ea1303c4aa0d34d7
anubhavc9 <anubhavc9@github.com>
2022-04-07 18:01:52 +0530

    changed URL in services

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,43 +1,64 @@
 import { Injectable } from '@angular/core';
-import { Todo } from "../Todo"
-import { HttpClient } from '@angular/common/http'
+import { Todo } from '../Todo';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
-
   todos: Todo[] = [];
   count: number;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('http://localhost:5000/todos');
+    // return this.http.get<Todo[]>('http://localhost:5000/todos');
+    return this.http.get<Todo[]>(
+      'https://my-todo-list-backend.herokuapp.com/todos'
+    );
   }
 
   addTodo(todo: Todo) {
-    return this.http.post<Todo>('http://localhost:5000/todos', {
-      "title": todo.title,
-      "description": todo.description,
-      "active": todo.active
-    });
+    // return this.http.post<Todo>('http://localhost:5000/todos', {
+    //   "title": todo.title,
+    //   "description": todo.description,
+    //   "active": todo.active
+    // });
+    return this.http.post<Todo>(
+      'https://my-todo-list-backend.herokuapp.com/todos',
+      {
+        title: todo.title,
+        description: todo.description,
+        active: todo.active,
+      }
+    );
   }
 
   deleteTodo(todo: Todo) {
-    return this.http.delete('http://localhost:5000/todos/'+todo.sno);
+    // return this.http.delete('http://localhost:5000/todos/'+todo.sno);
+    return this.http.delete(
+      'https://my-todo-list-backend.herokuapp.com/todos/' + todo.sno
+    );
   }
 
   toggleTodo(todo: Todo) {
     const index = this.todos.indexOf(todo);
     this.todos[index].active = !this.todos[index].active;
 
-    return this.http.put('http://localhost:5000/todos/'+todo.sno, {
-      "title": todo.title,
-      "description": todo.description,
-      "active": this.todos[index].active
-    });
+    // return this.http.put('http://localhost:5000/todos/' + todo.sno, {
+    //   title: todo.title,
+    //   description: todo.description,
+    //   active: this.todos[index].active,
+    // });
+    return this.http.put(
+      'https://my-todo-list-backend.herokuapp.com/todos/' + todo.sno,
+      {
+        title: todo.title,
+        description: todo.description,
+        active: this.todos[index].active,
+      }
+    );
   }
 
   remainingTodos(): number {
@@ -46,7 +67,7 @@ export class TodoService {
       if (todo.active == true) {
         this.count++;
       }
-    })
+    });
     return this.count;
   }
 }
